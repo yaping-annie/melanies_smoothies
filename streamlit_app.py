58becb970bf5c6018fa337eed39800fbf068c544
yaping-annie <130679866+yaping-annie@users.noreply.github.com>
2024-06-24 12:28:59 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
 
@@ -44,8 +45,5 @@ if ingredients_list:
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
-# New section to display fruityvice nutrition information
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
 fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
