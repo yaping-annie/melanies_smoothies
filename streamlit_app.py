49ed367485d52636cc722ce5af45b9045fe16b33
yaping-annie <130679866+yaping-annie@users.noreply.github.com>
2024-06-29 14:32:21 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,6 @@ if ingredients_list:
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
+st.subheader(fruit_chosen + 'Nutrition Information')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+fv_df = st.dataframe(data = fruityvice_response.json())
